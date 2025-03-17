de2b06b687aa067c9c8fb060bba3e4f97055b376
Abdullah Ademi <Abdullah.mughni1999@gmail.com>
2025-03-18 01:22:08 +0300

    adding graphql

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+export default {
+    graphql: {
+      config: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        landingPage: false, // disable Sandbox everywhere
+        depthLimit: 7,
+        amountLimit: 100,
+      },
+    },
+  };
