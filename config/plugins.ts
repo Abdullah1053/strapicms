75032fe3a571a5f4e653bb19dbc04a294d90003f
Abdullah Ademi <Abdullah.mughni1999@gmail.com>
2025-03-18 01:33:44 +0300

    deleteng old config

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,11 +1,1 @@
-export default {
-    graphql: {
-      config: {
-        endpoint: '/graphql',
-        shadowCRUD: true,
-        landingPage: false, // disable Sandbox everywhere
-        depthLimit: 7,
-        amountLimit: 100,
-      },
-    },
-  };
+export default () => ({});
